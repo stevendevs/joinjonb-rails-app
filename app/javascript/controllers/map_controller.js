00381a3c0e57e7c7a2a1b9609a7524d3dddbd01d
KevinAlpizarDev <kevinalpizardev@gmail.com>
2025-05-23 13:19:21 -0600

    Good results with map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -5,15 +5,15 @@ export default class extends Controller {
   static values = { x: Number, y: Number, data: Array }
 
   connect() {
-    let xMap = 51.505
-    let yMap = -0.09
+    let xMap = 9.92848244462257
+    let yMap = -84.09113133283863
 
     if ((this.xValue != "") && (this.yValue != "")) {
       xMap = this.xValue
       yMap = this.yValue
     }
 
-    this.map = L.map('map').setView([xMap, yMap], 13);
+    this.map = L.map('map').setView([xMap, yMap], 7);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
