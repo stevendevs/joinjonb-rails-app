31d640442f86d2440c71d240d5e0340ae9aba495
KevinAlpizarDev <kevinalpizardev@gmail.com>
2025-05-28 00:56:16 -0600

    Before fix md

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -2,24 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="mobile-menu"
 export default class extends Controller {
-  static targets = ["toggle", "menu"]
+  static targets = ["menu"]
 
   connect() {
-    this.setBodyPadding()
-    window.addEventListener("resize", this.setBodyPadding)
-  }
-
-  disconnect() {
-    window.removeEventListener("resize", this.setBodyPadding)
+    this.setInitialState()
   }
 
   toggleMenu() {
     this.menuTarget.classList.toggle("hidden")
   }
 
-  // Arrow function to keep `this` context
-  setBodyPadding = () => {
-    const isMobile = window.innerWidth < 640
-    document.body.style.paddingTop = isMobile ? "8rem" : "0"
+  setInitialState() {
+    if (window.innerWidth >= 640) {
+      this.menuTarget.classList.remove("hidden")
+    } else {
+      this.menuTarget.classList.add("hidden")
+    }
   }
 }
