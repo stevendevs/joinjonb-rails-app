01fde49f6860c3204767bdcdcd07c5f948d4302a
KevinAlpizarDev <kevinalpizardev@gmail.com>
2025-05-30 10:43:14 -0600

    Navbar like it can be

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -1,6 +1,6 @@
+// mobile_menu_controller.js
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="mobile-menu"
 export default class extends Controller {
   static targets = ["menu"]
 
